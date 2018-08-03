3d99999922b8065a328ad4ff996a4c873745a40e
mans <abilmansur.999@gmail.com>
2018-08-03 16:41:47 +0900

    adds deepspeech2 speech recognizer adds support of language model

--- assistant.py
+++ assistant.py
@@ -1,19 +1,42 @@
 import os
 from tkinter import *
 from tkinter.filedialog import askdirectory
-import wave
+import argparse
+import player
+import transcriber
 
-import player, transcriber
-
+# import wave
 # import pydub # open almost any format, slice
 # import pyaudio
 # maybe import pyglet
 # import pygame
 
+parser = argparse.ArgumentParser(description='DeepSpeech2 transcription')
+parser.add_argument('--model_path', default='deepspeech/models/librispeech_pretrained.pth',
+                    help='Path to model file created by training')
+parser.add_argument('--decoder', default="beam", choices=["greedy", "beam"], type=str, help="Decoder to use")
+parser.add_argument('--offsets', dest='offsets', action='store_true', help='Returns time offset information')
+beam_args = parser.add_argument_group("Beam Decode Options", "Configurations options for the CTC Beam Search decoder")
+beam_args.add_argument('--top_paths', default=1, type=int, help='number of beams to return')
+beam_args.add_argument('--beam_width', default=10, type=int, help='Beam width to use')
+beam_args.add_argument('--lm_path', default='deepspeech/lm/3-gram.pruned.1e-7.arpa', type=str,
+                       help='Path to an (optional) kenlm language model for use with beam search (req\'d with trie)')
+beam_args.add_argument('--alpha', default=0.8, type=float, help='Language model weight')
+beam_args.add_argument('--beta', default=1, type=float, help='Language model word bonus (all words)')
+beam_args.add_argument('--cutoff_top_n', default=40, type=int,
+                       help='Cutoff number in pruning, only top cutoff_top_n characters with highest probs in '
+                            'vocabulary will be used in beam search, default 40.')
+beam_args.add_argument('--cutoff_prob', default=1.0, type=float,
+                       help='Cutoff probability in pruning,default 1.0, no pruning.')
+beam_args.add_argument('--lm_workers', default=1, type=int, help='Number of LM processes to use')
+args = parser.parse_args()
+
+
 TranscriptDuration = 15
 
 PL = player.PlayList()
-T = transcriber.GoogleCloudTranscriber()
+# T = transcriber.GoogleCloudTranscriber()
+T = transcriber.DeepSpeech2Transcriber(args)
 
 def ask_playlist():
     # asks directory of a playlist and makes list of songs from it
